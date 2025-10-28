a680a7365b47df14d5876fadd39d9d2de38f01fe
olegovich2 <19922403wawa@gmail.com>
2025-10-28 13:38:25 +0300

    28/10/25

---
+++ App.js
@@ -0,0 +1,9 @@
+﻿import React from 'react';
+import ReactDOM from 'react-dom';
+
+import Warning from './components/Warning';
+
+ReactDOM.render(
+  <Warning question="Не трогайте мокрыми руками оголённые провода!" />, 
+  document.getElementById('container') 
+);
