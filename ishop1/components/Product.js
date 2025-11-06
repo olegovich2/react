c78af1965d002325301a35acae7d9122f04b6fbd
olegovich2 <19922403wawa@gmail.com>
2025-11-06 17:09:53 +0300

    ishop1

--- ishop1/components/Product.js
+++ ishop1/components/Product.js
@@ -4,18 +4,16 @@ import ProductLink from "./ProductLink";
 
 class Product extends React.Component {
   render() {
-    if (this.props.isLink) {
-      return (
-        <tr>
-          <td>{this.props.name}</td>
-          <td>{this.props.price}</td>
-          <td>{this.props.residual}</td>
-          <td>
-            <ProductLink url={this.props.url} title={this.props.name} />
-          </td>
-        </tr>
-      );
-    } else return null;
+    return (
+      <tr>
+        <td>{this.props.name}</td>
+        <td>{this.props.price}</td>
+        <td>{this.props.residual}</td>
+        <td>
+          <ProductLink url={this.props.url} title={this.props.name} />
+        </td>
+      </tr>
+    );
   }
 }
 
--- ishop1/components/Shop.js
+++ ishop1/components/Shop.js
@@ -3,24 +3,12 @@
 import "./Shop.css";
 
 import ShopCaption from "./ShopCaption";
-import TitleColumns from "./TitleColumns";
 import Product from "./Product";
 
 class Shop extends React.Component {
   render() {
-    const answersCode = this.props.answers.map((v) => (
-      <TitleColumns
-        isLink={v.isLink}
-        key={v.code}
-        name={v.name}
-        price={v.price}
-        residual={v.residual}
-        url={v.url}
-      />
-    ));
-    const answersCodeForTbody = this.props.answers.map((v) => (
+    const answersCodeForProduct = this.props.answers.map((v) => (
       <Product
-        isLink={v.isLink}
         key={v.code}
         name={v.name}
         price={v.price}
@@ -31,8 +19,15 @@ class Shop extends React.Component {
     return (
       <table>
         <ShopCaption caption={this.props.caption} />
-        <thead>{answersCode}</thead>
-        <tbody>{answersCodeForTbody}</tbody>
+        <thead>
+          <tr>
+            <th>Название товара</th>
+            <th>Цена товара</th>
+            <th>Остаток на складе</th>
+            <th>Изображение</th>
+          </tr>
+        </thead>
+        <tbody>{answersCodeForProduct}</tbody>
       </table>
     );
   }
