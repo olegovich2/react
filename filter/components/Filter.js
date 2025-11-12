459d458a48658f234967cda54921f48de421c787
olegovich2 <19922403wawa@gmail.com>
2025-11-12 12:38:14 +0300

    filter

--- filter/components/Filter.js
+++ filter/components/Filter.js
@@ -23,68 +23,76 @@ class Filter extends React.Component {
     };
 
     const sortAndFilter = (eo) => {
-      if (eo.target.name === "checkbox") {
-        this.state.isChecked = eo.target.checked;
-      } else if (
-        eo.target.name === "inputForFilter" &&
-        eo.target.value.length === 0
-      ) {
-        this.state.letter = eo.target.value;
-        this.state.isFilter = false;
-      } else if (
-        eo.target.name === "inputForFilter" &&
-        eo.target.value.length > 0
-      ) {
-        this.state.letter = eo.target.value;
-        this.state.isFilter = true;
+      switch (eo.target.name) {
+        case "checkbox":
+          this.state.isChecked = eo.target.checked;
+          break;
+        case "inputForFilter":
+          if (eo.target.value.length === 0) {
+            this.state.letter = eo.target.value;
+            this.state.isFilter = false;
+          } else if (eo.target.value.length > 0) {
+            this.state.letter = eo.target.value;
+            this.state.isFilter = true;
+          }
+          break;
       }
 
-      if (this.state.isChecked && !this.state.isFilter) {
-        const sortList = [...this.state.originList].sort();
-        this.setState({ sortList: sortList });
-      } else if (!this.state.isChecked && this.state.isFilter) {
-        const filterList = [...this.state.originList].filter((element) =>
-          element.includes(this.state.letter)
-        );
-        this.setState({ filterList: filterList });
-      } else if (this.state.isChecked && this.state.isFilter) {
-        if (this.state.sortList.length === 0) {
-          const originCopy = [...this.props.listWords];
-          const mixList = [...this.state.filterList].sort();
-          this.setState({
-            mixList: mixList,
-            filterList: originCopy.filter((element) =>
-              element.includes(this.state.letter)
-            ),
-          });
-        } else {
-          const originCopy = [...this.props.listWords];
-          const mixList = [...this.state.sortList].filter((element) =>
+      const stateCode =
+        (this.state.isChecked ? 1 : 0) + (this.state.isFilter ? 2 : 0);
+
+      switch (stateCode) {
+        case 1: // isChecked: true, isFilter: false
+          const sortList = [...this.state.originList].sort();
+          this.setState({ sortList: sortList });
+          break;
+        case 2: // isChecked: false, isFilter: true
+          const filterList = [...this.state.originList].filter((element) =>
             element.includes(this.state.letter)
           );
-          this.setState({ mixList: mixList, sortList: originCopy.sort() });
-        }
-      } else {
-        this.setState({
-          originList: this.props.listWords,
-        });
+          this.setState({ filterList: filterList });
+          break;
+        case 3: // isChecked: true, isFilter: true
+          if (this.state.sortList.length === 0) {
+            const originCopy = [...this.props.listWords];
+            const mixList = [...this.state.filterList].sort();
+            this.setState({
+              mixList: mixList,
+              filterList: originCopy.filter((element) =>
+                element.includes(this.state.letter)
+              ),
+            });
+          } else {
+            const originCopy = [...this.props.listWords];
+            const mixList = [...this.state.sortList].filter((element) =>
+              element.includes(this.state.letter)
+            );
+            this.setState({ mixList: mixList, sortList: originCopy.sort() });
+          }
+          break;
+        case 0: // isChecked: false, isFilter: false
+        default:
+          this.setState({
+            originList: this.props.listWords,
+          });
+          break;
       }
     };
 
-    if (!this.state.isChecked && !this.state.isFilter) {
-      wordFromList = this.state.originList.map((v, index) => (
-        <Word value={v} key={index} />
-      ));
-    } else if (this.state.isChecked && !this.state.isFilter) {
-      wordFromList = this.state.sortList.map((v, index) => (
-        <Word value={v} key={index} />
-      ));
-    } else if (!this.state.isChecked && this.state.isFilter) {
-      wordFromList = this.state.filterList.map((v, index) => (
-        <Word value={v} key={index} />
-      ));
-    } else if (this.state.isChecked && this.state.isFilter) {
-      wordFromList = this.state.mixList.map((v, index) => (
+    const cases = [
+      { checked: false, filter: false, list: "originList" },
+      { checked: true, filter: false, list: "sortList" },
+      { checked: false, filter: true, list: "filterList" },
+      { checked: true, filter: true, list: "mixList" },
+    ];
+
+    const currentCase = cases.find(
+      (c) =>
+        c.checked === this.state.isChecked && c.filter === this.state.isFilter
+    );
+
+    if (currentCase) {
+      wordFromList = this.state[currentCase.list].map((v, index) => (
         <Word value={v} key={index} />
       ));
     }
