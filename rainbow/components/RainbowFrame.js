bdb12a86c9c9bdd94dd64ade05dad3a44ddedef3
olegovich2 <19922403wawa@gmail.com>
2025-11-21 13:03:37 +0300

    rainbow

--- rainbow/components/RainbowFrame.js
+++ rainbow/components/RainbowFrame.js
@@ -1,26 +1,32 @@
 import React, { Component } from "react";
 
 class RainbowFrame extends Component {
-  createFrames = (colors, content) => {
-    if (colors.length === 0) return <div>{content}</div>;
-    const currentColor = colors[0];
-    const remainingColors = colors.slice(1);
-    return (
-      <div
-        style={{
-          border: `10px solid ${currentColor}`,
-          margin: "5px",
-          textAlign: "center",
-        }}
-      >
-        {this.createFrames(remainingColors, content)}
-      </div>
-    );
-  };
-
   render() {
     const { colors, children } = this.props;
-    return this.createFrames(colors, children);
+
+    if (!colors || colors.length === 0) {
+      return <div>{children}</div>;
+    }
+
+    let content = children;
+
+    for (let i = 0; i < colors.length; i++) {
+      content = (
+        <div
+          style={{
+            border: `10px solid ${colors[i]}`,
+            padding: "10px",
+            margin: "5px",
+            textAlign: "center",
+            borderRadius: "5px",
+          }}
+        >
+          {content}
+        </div>
+      );
+    }
+
+    return content;
   }
 }
 
